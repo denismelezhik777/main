17dadaee820a9a7a96f441b71d0a776c14e60231
Denis Alecsandrovich <den.melezhik@inbox.ru>
2021-12-15 16:13:48 +1000

    begin

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 TEMP = 0
+PRUT = 0
 
 @app.get("/")
 async def root():
-    return TEMP
+    global PRUT, TEMP
+    PRUT += 1
+    return PRUT, TEMP
 
 
 @app.post("/webhook")
 async def temperoutput(item: dict):
+    global TEMP
     TEMP = item
     print(item)
     return {"OK": True}
